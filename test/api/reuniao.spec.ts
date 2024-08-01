714db0217fc2b3d52945710199edcf96d106d5cf
Ygor Cruz <ygor.felix@fornecedores.sicoob.com.br>
2024-08-01 17:57:15 -0300

    test: criado primeiro teste

--- test/api/reuniao.spec.ts
+++ test/api/reuniao.spec.ts
@@ -1,5 +1,30 @@
+const request = require("request");
+const PORT = process.env.TEST_PORT || 8080;
+const HOST = process.env.TEST_HOST || "localhost";
+const urlBase = `http://${HOST}:${PORT}/api`;
+const recurso = "reuniao";
+
 describe("/reuniao", () => {
-  it("deveria funcionar", () => {
-    expect("simples test").toBeTruthy();
+  it("deveria retornar uma lista vazia na primera request", (done) => {
+    request.get(
+      {
+        url: `${urlBase}/${recurso}`,
+        json: true,
+      },
+      (err, response) => {
+        if (err) {
+          done(err);
+          return;
+        }
+
+        try {
+          expect(response.body).toHaveLength(0);
+
+          done();
+        } catch (e) {
+          done(e);
+        }
+      },
+    );
   });
 });
