d7181d2234809bd68db4b3b25be84e7dc3efd44d
Ygor Cruz <ygor.felix@fornecedores.sicoob.com.br>
2024-08-02 16:17:13 -0300

    test: corrigidos bugs de build

--- jest.config.ts
+++ jest.config.ts
@@ -146,7 +146,7 @@ const config: Config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -175,7 +175,9 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
